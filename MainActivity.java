5ad3a0ad8fb32b87c38fe684b5fc14247a69831c
Karthik-V-g <68321621+Karthik-V-g@users.noreply.github.com>
2022-03-28 00:51:28 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.services;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaParser;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button start,stop;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        start=(Button) findViewById(R.id.play);
+        stop=(Button) findViewById(R.id.stop);
+        
+        start.setOnClickListener(this);
+        stop.setOnClickListener(this);
+       
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view==start){
+            startService(new Intent(this,myservices.class));
+        }
+        else if(view==stop){ stopService(new Intent(this,myservices.class));}
+    }
+}
